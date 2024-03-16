085eced680307cb2297af8adb5f4c644eaa82bc0
whatislavs <vlady.lu@gmail.com>
2024-03-16 13:30:12 -0400

    created the initial budget allocation app

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,29 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget, dispatch } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+    const handleSaveBudget = () => {
+        // Dispatch action to update budget in context
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: parseFloat(newBudget), // Ensure newBudget is a number
+        });
+    };
+    const handleKeyDown = (event) => {
+        if(event.key === 'Enter') {
+            handleSaveBudget();
+        }
+    };
+    return (
+<div className='alert alert-secondary'>
+<span>Budget: £{budget}</span>
+<input type="number" step="10" value={newBudget} onChange={handleBudgetChange} onBlur={handleSaveBudget} onKeyDown={handleKeyDown}></input>
+</div>
+    );
+};
+export default Budget;
